1ec13ec113bd0bee1e411b272aec3ee45daba50b
Rapina <163008675+rapinadescolado@users.noreply.github.com>
2025-09-29 09:11:45 -0300

    .

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+// Set elements
+const menu = document.getElementById("menu");
+const display = document.getElementById("display");
+const mapadobrasil = document.getElementById("menu");
+const cristoredentor = document.getElementById("display");
